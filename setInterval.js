c31d2d1540bc89aa42f8c8ce41295920df465476
shivrajMp <spattanshetti81@gmail.com>
2024-02-29 05:04:20 +0530

    setinterval

---
+++ setInterval.js
@@ -0,0 +1,39 @@
+// predict output
+
+console.log("1");
+ 
+setTimeout(() => {
+
+   console.log("2");
+
+})
+
+
+console.log("3");
+
+
+setTimeout(() => {
+
+   console.log("4");
+
+},0)
+
+
+console.log("5");
+
+setTimeout(() => {
+
+   console.log("6");
+
+},1000)
+
+
+console.log("7");
+
+
+setTimeout(() => {
+
+   console.log("8");
+
+},500)
+// 1 3 5 7 2 4 8 6
